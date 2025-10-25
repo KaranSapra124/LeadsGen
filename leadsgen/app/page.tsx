b4a6f0696198d0f74e2584499e45ecde9d12f272
Karan Sapra <sapra7112@gmail.com>
2025-10-25 14:13:40 +0530

    Added rediretion in root path

--- leadsgen/app/page.tsx
+++ leadsgen/app/page.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
-
+import {redirect} from "next/navigation"
 export default function Home() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+  redirect("/Routes/Dashboard/Lead")
   );
 }
